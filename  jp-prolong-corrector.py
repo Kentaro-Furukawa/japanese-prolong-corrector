c38e78175668ceaf23c011513823c0d51694aeb4
Ken <info@ken89.com>
2021-12-05 10:38:07 +0900

    first commit

---
+++  jp-prolong-corrector.py
@@ -0,0 +1,21 @@
+import unicodedata
+
+def jp_prolong(string):
+    hyphen = "-"
+    correct_string = ""
+    for target in string:
+        if target == hyphen and before_target != "Na":
+            item = "ー"
+        else:
+            item = target
+        before_target = unicodedata.east_asian_width(target)
+        correct_string += item
+    return correct_string
+
+the_sheep_password = "バーラーミュー"
+
+print(jp_prolong(the_sheep_password))
+
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+#Correcting misused hyphen to japanese prolong.
+
+Dealing with japanese character is a so much hassle.
+this function correct misused hyphen to japanese prolong.
